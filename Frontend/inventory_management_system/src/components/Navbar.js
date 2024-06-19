4a6941a34183fc4c9a2578acfdc77864e886709c
jyothi-student <jy5088010@gmail.com>
2024-06-19 11:29:36 +0100

    weblab

--- Frontend/inventory_management_system/src/components/Navbar.js
+++ Frontend/inventory_management_system/src/components/Navbar.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 export default function Navbar(props) {
   return (
     <div>
-      <navBar className="navbar navbar-expand-lg bg-danger">
+      <nav className="navbar navbar-expand-lg" style={{ backgroundColor: '#937c91' }}>
         <div className="container-fluid">
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -11,23 +11,15 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active text-white fs-4" aria-current="page" href="/">{props.title}</a>
+                <a className="nav-link active text-white fs-4" aria-current="page" href="/">Home</a>
               </li>
               <li className="nav-item">
                 <a className="nav-link active text-white fs-4" aria-current="page" href="/products">Products</a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link active text-white fs-4" aria-current="page" href="/about">About</a>
-              </li>
             </ul>
-            <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-primary fs-5" type="submit">Search</button>
-            </form>
           </div>
         </div>
-
-      </navBar>
+      </nav>
     </div>
-  )
+  );
 }
